c43239d0f1f3c909171bf2ddb7670b31f8db464a
Ohimoiza1205 <163584196+Ohimoiza1205@users.noreply.github.com>
2024-03-21 00:09:01 -0500

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,3 +1,5 @@
+-- Create a stored procedure ComputeAverageScoreForUser that
+-- computes and stores the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
