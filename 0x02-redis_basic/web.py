12232f43c533e88fc23c9c04aa842d4d4df3314b
Ohimoiza1205 <163584196+Ohimoiza1205@users.noreply.github.com>
2024-03-27 23:44:29 -0500

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,6 +4,13 @@
 the function is very simple. It uses the requests module
 to obtain the HTML content of a particular URL and returns it.
 
+Start in a new file named web.py and do not reuse the code
+written in exercise.py.
+
+Inside get_page track how many times a particular URL was
+accessed in the key "count:{url}" and cache the result with
+an expiration time of 10 seconds.
+
 Tip: Use http://slowwly.robertomurray.co.uk to simulate
 a slow response and test your caching."""
 
